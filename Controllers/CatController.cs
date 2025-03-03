f1b249a1435ef36289e67e125ce4b62d5e125322
lib 4qq <nlasted@outlook.com>
2025-03-03 17:41:11 +0200

    Add project files.

---
+++ Controllers/CatController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.DAL;
+using WebApplication1.DAL.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Controller]
+    [Route("/api/cat")]
+    public class CatController : ControllerBase
+    {
+        private AppDbContext context;
+
+        public CatController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var models = context.Cats.ToList();
+
+            return Ok(models);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Cat model)
+        {
+            context.Cats.Add(model);
+            context.SaveChanges();
+
+            return Ok("Cat created");
+        }
+    }
+}
